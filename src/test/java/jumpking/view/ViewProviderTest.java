8993f8ee757a8681cddf712d419e82be9951dccb
joanadcarvalhal <joana.d.carvalhal@gmail.com>
2024-11-25 02:21:56 +0000

    ViewProviderTest

---
+++ src/test/java/jumpking/view/ViewProviderTest.java
@@ -0,0 +1,37 @@
+package jumpking.view;
+
+import jumpking.view.elements.KingViewer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+public class ViewProviderTest {
+    private SpriteLoader spriteLoader;
+    private ViewProvider viewProvider;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        this.spriteLoader = Mockito.mock(SpriteLoader.class);
+        viewProvider = new ViewProvider(spriteLoader);
+    }
+
+    @Test
+    public void kingViewerTest() {
+        KingViewer kingViewer = viewProvider.getKingViewer();
+        assertNotNull(kingViewer);
+        verify(spriteLoader, times(1)).getSprite("sprites/king-idle.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-fallen.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-crouching.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-jumping.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-falling.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-running-1.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-running-2.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-running-3.png");
+        verify(spriteLoader, times(1)).getSprite("sprites/king-rebound.png");
+    }
+}
